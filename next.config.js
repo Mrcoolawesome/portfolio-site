8c27d0dca7c107726d45930186101fc3186c5790
Devin Schutz <schutzdevin@gmail.com>
2026-03-28 23:24:34 -0600

    fixed maybe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': [
+        'GASTeamStuff/**/*',
+        'OarWeThereYetStuff/**/*',
+        'RoboticsStuff/**/*',
+        '*.jpg',
+        '*.png',
+        '*.gif',
+        '*.mp4',
+        '*.webm'
+      ],
+    },
+  },
 }
 
 module.exports = nextConfig
